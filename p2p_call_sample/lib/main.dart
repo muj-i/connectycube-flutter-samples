94a76292eea4a069c90087957b39bb8f92928b38
Mujahedul Islam <dev.muj.i@icloud.com>
2024-04-21 14:26:13 +0600

    remove oss

--- p2p_call_sample/lib/main.dart
+++ p2p_call_sample/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:connectycube_flutter_call_kit/connectycube_flutter_call_kit.dart';
 import 'package:connectycube_sdk/connectycube_sdk.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:p2p_call_sample/firebase_options.dart';
 
 import 'src/login_screen.dart';
 import 'src/utils/configs.dart' as config;
 import 'src/utils/pref_util.dart';
 
-void main() => runApp(App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(App());
+}
 
 class App extends StatefulWidget {
   @override
--- p2p_call_sample/macos/Flutter/GeneratedPluginRegistrant.swift
+++ p2p_call_sample/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import firebase_core
 import flutter_webrtc
 import package_info_plus
 import path_provider_foundation
@@ -13,6 +14,7 @@ import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
